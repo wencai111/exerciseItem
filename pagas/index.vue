56d370ca1b648fbb7ccd81a1eee5a1544432496d
wencai111 <1259310290@qq.com>
2019-04-22 11:05:12 +0800

    第二次提交

--- pagas/index.vue
+++ pagas/index.vue
@@ -1,7 +1,21 @@
 <template>
-	<view>我的第一次提交</view>
+		<view class="content">
+		<view class="example">
+			<view>
+				<view class="uni-card">
+					<view class="">
+						<view class="wxc-list-extra">123</view>
+						<view class="wxc-list-extra"></view>
+						<view class="wxc-list-extra"></view>
+						<view class="wxc-list-extra"></view>
+						<view class="wxc-list-extra"></view>
+						</view>
+						</view>
+		</view>
+	</view>
 </template>
 <script>
+	import {uniCard} from '@dcloudio/uni-ui';
 </script>
 
 <style>
